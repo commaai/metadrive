a201fe019fbc96cb03068256e77eba0d3765e63a
Maxime Desroches <desroches.maxime@gmail.com>
2024-12-19 10:56:01 -0800

    what is this, 1990?

--- metadrive/third_party/simplepbr/shaders/tonemap.frag.glsl
+++ metadrive/third_party/simplepbr/shaders/tonemap.frag.glsl
@@ -1,13 +1,11 @@
-#version 120
+#version 330
 
 uniform sampler2D tex;
 uniform float exposure;
 
 varying vec2 v_texcoord;
 
-#ifdef USE_330
 out vec4 o_color;
-#endif
 
 void main() {
     vec3 color = texture(tex, v_texcoord).rgb;
@@ -16,9 +14,5 @@ void main() {
     color = max(vec3(0.0), color - vec3(0.004));
     color = (color * (vec3(6.2) * color + vec3(0.5))) / (color * (vec3(6.2) * color + vec3(1.7)) + vec3(0.06));
 
-#ifdef USE_330
     o_color = vec4(color, 1.0);
-#else
-    gl_FragColor = vec4(color, 1.0);
-#endif
 }
